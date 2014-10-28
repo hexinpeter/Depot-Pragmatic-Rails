6b36e80cfb6809aae1e0d06208d1a38fb53a7a32
Peter He <hexinpeter@gmail.com>
2014-10-28 23:44:17 +1100

    finished Chapter 8

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -22,6 +22,7 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select 'a', 'Show'
   end
 
   test "should get new" do
